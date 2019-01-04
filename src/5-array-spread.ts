43c7abaa211edb8bdab961ca9203af12ac2be90f
svroye <vanroyesteven@gmail.com>
2019-01-04 10:53:00 +0100

    spread operator

---
+++ src/5-array-spread.ts
@@ -0,0 +1,15 @@
+/**
+ * ARRAY SPREAD OPERATOR
+ */
+
+/**
+ const toppings = ['bacon', 'chili'];
+
+ const newToppings = ['pepperoni'];
+
+ // the spread operator creates a reference to the old arrays 
+ // and put them in a new array
+ const allToppings = [...toppings, ... newToppings];
+
+ console.log(allToppings);
+  */
